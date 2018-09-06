24262f18e1d54385fc2ba137bff8d35d5486bbb8
Kenton Hamaluik <kenton@rehabtronics.com>
2018-09-06 16:44:53 -0600

    added loading and parsing todo.txt, simple display

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,80 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate toml;
 extern crate clap;
 extern crate home;
 extern crate todo_txt;
+extern crate crossterm;
 
+use std::error::Error;
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::PathBuf;
+use std::io::BufReader;
+use std::io::BufRead;
 use clap::{App, Arg, SubCommand};
+use todo_txt::Task;
+use crossterm::{Screen, Crossterm, Color};
+
+#[derive(Deserialize)]
+struct Config {
+    path: String,
+}
+
+fn ensure_config_and_get_contents(path: &PathBuf) -> String {
+    let display = path.display();
+
+    if !path.exists() {
+        let mut file = match File::create(&path) {
+            Err(why) => panic!("couldn't create {}: {}", display, why.description()),
+            Ok(file) => file,
+        };
+
+        let todo_loc = match home::home_dir() {
+            Some(v) => v.join("todo.txt"),
+            None => panic!("Couldn't find home!")
+        };
+
+        if let Err(why) = file.write_all(format!("path = '{}'", todo_loc.to_str().unwrap()).as_bytes()) {
+            panic!("couldn't write to {}: {}", display, why.description())
+        }
+    }
+
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why.description()),
+        Ok(file) => file,
+    };
+
+    let mut contents: String = String::new();
+    if let Err(why) = file.read_to_string(&mut contents) {
+        panic!("couldn't read from {}: {}", display, why.description())
+    }
+    contents
+}
+
+fn ensure_todo_file_exists(path: &PathBuf) {
+    if !path.exists() {
+        let display = path.display();
+        if let Err(why) = File::create(&path) {
+            panic!("couldn't create {}: {}", display, why.description())
+        }
+    }
+}
+
+fn parse_todo_txt(path: &PathBuf) -> Vec<Task> {
+    let display = path.display();
+    let f = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why.description()),
+        Ok(file) => file,
+    };
+    let file = BufReader::new(&f);
+    file.lines().map(|line| todo_txt::parser::task(&line.unwrap()).unwrap()).collect()
+}
+
+fn print_task(screen: &Screen, crossterm: &Crossterm, task: &Task) {
+    let style = crossterm.style(format!("{}\n", task.subject)).with(Color::Black).on(Color::White);
+    style.paint(&screen);
+}
 
 fn main() {
     let app: App = App::new("todo.txt")
@@ -37,20 +109,35 @@ fn main() {
             .help("override the todo.txt path from the config")
             .takes_value(true)
         );
-    
     let matches = app.get_matches();
-    let config: String = match matches.value_of("config") {
-        Some(c) => String::from(c),
+
+    let screen = Screen::default();
+    let crossterm = Crossterm::new(&screen);
+
+    // parse the config
+    let config: PathBuf = match matches.value_of("config") {
+        Some(c) => PathBuf::from(c),
         None => {
             match home::home_dir() {
-                Some(v) => String::from(v.to_str().unwrap()),
+                Some(v) => v.join(".todo.toml"),
                 None => {
                     panic!("No config path supplied and couldn't find home!");
                 }
             }
         }
     };
-    println!("Using config: {}", config);
+    let config = ensure_config_and_get_contents(&config);
+    let config: Config = match toml::from_str(&config) {
+        Ok(c) => c,
+        Err(why) => panic!("couldn't parse config: {}", why.description())
+    };
+
+    // parse the path override
+    let path: PathBuf = match matches.value_of("path") {
+        Some(p) => PathBuf::from(p),
+        None => PathBuf::from(config.path)
+    };
+    ensure_todo_file_exists(&path);
     
     if let Some(_) = matches.subcommand_matches("add") {
         // TODO: add
@@ -59,6 +146,10 @@ fn main() {
         // TODO: search
     }
     else {
-        // TODO: list all our tasks!
+        // list all our tasks by default
+        let tasks = parse_todo_txt(&path);
+        for task in tasks {
+            print_task(&screen, &crossterm, &task);
+        }
     }
 }
